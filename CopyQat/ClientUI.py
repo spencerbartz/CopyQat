53541ea5958441dcfd4bb55573d1138b13870f92
Spencer <spencer.bartz@healthsparq.com>
2016-12-25 20:08:55 -0800

    finish converting from CopyCat to CopyQat. add ability to delete selected files in selection box

--- CopyQat/ClientUI.py
+++ CopyQat/ClientUI.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageTk
 from Tkinter import Tk, Text, RIGHT, BOTH, RAISED, X, Y, N, W, LEFT, Listbox, END
 from ttk import Frame, Button, Style, Label, Entry
-from CopyCat import Client
+from CopyQat import Client
 import ttk
 import tkFont
 import tkFileDialog
@@ -35,7 +35,7 @@ class ClientUI(Frame):
         fileLabel = Label(file_frame, text="Files", width=6)
         fileLabel.pack(side=LEFT, anchor=N, padx=5, pady=5)
 
-        self.file_list = Listbox(file_frame)
+        self.file_list = Listbox(file_frame, selectmode='multiple')
         self.file_list.pack(side=LEFT, fill=BOTH, pady=5, padx=(0,50), expand=True)
 
         # Create a frame for buttons and attach to self
@@ -45,6 +45,8 @@ class ClientUI(Frame):
         # Attach buttons to button_frame. padx can take a 2-tuple for left and right padding
         close_button = Button(button_frame, text="Exit", command=self.quit)
         close_button.pack(side=RIGHT, padx=(5, 50))
+        remove_button = Button(button_frame, text="Remove Files", command=self.remove_files)
+        remove_button.pack(side=RIGHT, padx=(5, 5))
         send_button = Button(button_frame, text="Send Files", command=self.send_files)
         send_button.pack(side=RIGHT, padx=(5, 5))
         ok_button = Button(button_frame, text="Add File(s)", command=self.add_file_to_queue)
@@ -61,19 +63,26 @@ class ClientUI(Frame):
         y = (screen_height - height) / 2
         self.parent.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
+    def remove_files(self):
+        # Delete from Listbox
+        indices = list(self.file_list.curselection())
+        indices.reverse()
+        for i in indices:
+            self.file_list.delete(i)
+
+    def send_files(self):
+        client = Client.Client()
+        client.connect("127.0.0.1", 8181)
+        file_names = list(self.file_list.get(0, self.file_list.size() - 1))
+        client.open_files(file_names)
+        client.read_files()
+        client.send_files()
+
     # Add the file name in string form to file_list
     def add_file_to_queue(self):
         file_opt_files = tkFileDialog.askopenfilenames()
         for f in file_opt_files:
             self.file_list.insert(END, f)
-
-    def send_files(self):
-        client = Client.Client()
-        client.connect("127.0.0.1", 8181)
-        file_names = list(self.file_list.get(0,self.file_list.size() - 1))
-        client.open_files(file_names)
-        client.read_files()
-        client.send_files()
 
     def start(self):
         self.parent.mainloop()
